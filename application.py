aa32134df1bdae9f26d057f58af7127d182b330b
Hector <hectorwilson1@yahoo.com>
2018-07-30 14:45:30 -0400

    mamamia

--- application.py
+++ application.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_restplus import Resource, Api
 
-# lalalla mooo
 # welcome to flask: http://flask.pocoo.org/
-# working with sqlalchemy & swagger:
+# working with alchemy & swagger:
 # http://michal.karzynski.pl/blog/2016/06/19/building-beautiful-restful-apis-using-flask-swagger-ui-flask-restplus/
 application = Flask(__name__)
 api = Api(application)
