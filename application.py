fca51d9bfd01ec28f477a55d50c3348408ffa6b4
Hector <hectorwilson1@yahoo.com>
2018-07-31 16:59:30 -0400

    Merge remote-tracking branch 'origin/development' into connie-orange-iguanas
    # Conflicts:
    #	application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
 
 
 # welcome to flask: http://flask.pocoo.org/
@@ -11,12 +11,20 @@ application = Flask(__name__)
 api = Api(application)
 with open('pokemon.json') as json_file:
     data = json.load(json_file)
+for x in data:
+    print(x["types"])
 
 
 @api.route("/pokemon/<string:pokemon>")                   # Create a URL route to this resource
 class HelloWorld(Resource):            # Create a RESTful resource
     def get(self, pokemon):                     # Create GET endpoint
         return data.get(pokemon)
+
+
+@api.route("/type/<string:type>")
+class HelloMars(Resource):
+    def get(self):
+        return y
 
 
 def main():
