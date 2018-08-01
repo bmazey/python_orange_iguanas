201dba7ebab6f9b42641b674d05bdf18c1e7d119
connie <conniezhuyx@gmail.com>
2018-08-01 12:09:44 -0400

    most of them are done

--- application.py
+++ application.py
@@ -1,25 +1,134 @@
+
 import json
 from flask import Flask, Response
 from flask_restplus import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_restplus import fields
 
 
 # welcome to flask: http://flask.pocoo.org/
 # working with sqlalchemy & swagger:
 # http://michal.karzynski.pl/blog/2016/06/19/building-beautiful-restful-apis-using-flask-swagger-ui-flask-restplus/
-# hello world
+# hello world2
 application = Flask(__name__)
 api = Api(application)
 with open('pokemon.json') as json_file:
     data = json.load(json_file)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(application)
 
 
 @api.route("/pokemon/<string:pokemon>")                   # Create a URL route to this resource
-class Pokemon(Resource):                               # Create a RESTful resource
+class Pokemon(Resource):                                  # Create a RESTful resource
     def get(self, pokemon):                               # Create GET endpoint
         return data.get(pokemon)
 
 
-@api.route("/pokemon/special_defense/<int:special_defense>")
+@api.route("/pokemon/types/<string:types>/<string:types2>")
+class Pokemon2Types(Resource):
+    def get(self, types, types2):
+        pokemon_names = ''
+        for name in data:
+            if [types, types2] or [types2, types] == data[name]["types"]:
+                pokemon_names += name
+                pokemon_names += ' '
+
+        pokemon_dict = {}
+        for names in pokemon_names.split():
+            pokemon_dict.update({names: data.get(names)})
+
+        json_format = json.dumps(pokemon_dict)
+        return Response(response=json_format, mimetype="application/json", status=200)
+
+
+# print(PokemonTypes().get(types="grass", types2="ice"))
+
+@api.route("/pokemon/types/<string:types>")
+class PokemonTypes(Resource):
+    def get(self, types):
+        pokemon_names = ''
+        for name in data:
+            if [types] == data[name]["types"]:
+                pokemon_names += name
+                pokemon_names += ' '
+
+        pokemon_dict = {}
+        for names in pokemon_names.split():
+            pokemon_dict.update({names: data.get(names)})
+
+        json_format = json.dumps(pokemon_dict)
+        return Response(response=json_format, mimetype="application/json", status=200)
+
+
+@api.route("/pokemon/stats/hp/<int:hp>")
+class PokemonHP(Resource):
+    def get(self, hp):
+        pokemon_names = ''
+        for name in data:
+            if hp == data[name]["stats"]["hp"]:
+                pokemon_names += name
+                pokemon_names += ' '
+
+        pokemon_dict = {}
+        for names in pokemon_names.split():
+            pokemon_dict.update({names: data.get(names)})
+
+        json_format = json.dumps(pokemon_dict)
+        return Response(response=json_format, mimetype="application/json", status=200)
+
+
+@api.route("/pokemon/stats/attack/<int:attack>")
+class PokemonAttack(Resource):
+    def get(self, attack):
+        pokemon_names = ''
+        for name in data:
+            if attack == data[name]["stats"]["attack"]:
+                pokemon_names += name
+                pokemon_names += ' '
+
+        pokemon_dict = {}
+        for names in pokemon_names.split():
+            pokemon_dict.update({names: data.get(names)})
+
+        json_format = json.dumps(pokemon_dict)
+        return Response(response=json_format, mimetype="application/json", status=200)
+
+
+@api.route("/pokemon/stats/defense/<int:defense>")
+class PokemonDefense(Resource):
+    def get(self, defense):
+        pokemon_names = ''
+        for name in data:
+            if defense == data[name]["stats"]["defense"]:
+                pokemon_names += name
+                pokemon_names += ' '
+
+        pokemon_dict = {}
+        for names in pokemon_names.split():
+            pokemon_dict.update({names: data.get(names)})
+
+        json_format = json.dumps(pokemon_dict)
+        return Response(response=json_format, mimetype="application/json", status=200)
+
+
+@api.route("/pokemon/stats/special_attack/<int:special_attack>")
+class PokemonSpecialAttack(Resource):
+    def get(self, special_attack):
+        pokemon_names = ''
+        for name in data:
+            if special_attack == data[name]["stats"]["special_attack"]:
+                pokemon_names += name
+                pokemon_names += ' '
+
+        pokemon_dict = {}
+        for names in pokemon_names.split():
+            pokemon_dict.update({names: data.get(names)})
+
+        json_format = json.dumps(pokemon_dict)
+        return Response(response=json_format, mimetype="application/json", status=200)
+
+
+@api.route("/pokemon/stats/special_defense/<int:special_defense>")
 class PokemonSpecialDefense(Resource):
     def get(self, special_defense):
         pokemon_names = ''
@@ -36,27 +145,23 @@ class PokemonSpecialDefense(Resource):
         return Response(response=json_format, mimetype="application/json", status=200)
 
 
-@api.route("/pokemon/attacks/<int:attacks>")
-class PokemonAttacks(Response):
-    def get(self, attacks):
+@api.route("/pokemon/stats/speed/<int:speed>")
+class PokemonSpeed(Resource):
+    def get(self, speed):
         pokemon_names = ''
         for name in data:
-            if attacks == data[name]["stats"]["attacks"]:
+            if speed == data[name]["stats"]["speed"]:
                 pokemon_names += name
                 pokemon_names += ' '
 
-'''
         pokemon_dict = {}
         for names in pokemon_names.split():
             pokemon_dict.update({names: data.get(names)})
 
         json_format = json.dumps(pokemon_dict)
         return Response(response=json_format, mimetype="application/json", status=200)
-'''
 
-print(PokemonAttacks().get(attacks=60))
 
-'''
 def main():
     application.debug = True
     application.run()
@@ -64,4 +169,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-'''
+
