c3bdfb50895a661b2b2280dddca4869cf1ba8c72
connie <conniezhuyx@gmail.com>
2018-08-02 14:58:56 -0400

    adding comments

--- test_app.py
+++ test_app.py
@@ -7,54 +7,63 @@ class RumorMillTest(TestCase):
         self.application = get_app()
         self.client = self.application.test_client()
 
+    # Test to get pokemon's status by input a name
     def test_get_pokemon(self):
         response = self.client.get("/pokemon/Pikachu")
         # print(response.get_json().get('types'))
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('types') == ['electric'])
 
+    # Test to get pokemon's status by input a type
     def test_get_pokemon_type(self):
         response = self.client.get("/pokemon/types/bug")
         # print(response.get_json().get('Accelgor').get('types'))
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Accelgor').get('types') == ['bug'])
 
+    # Test to get pokemon's status by input two types
     def test_get_pokemon_two_types(self):
         response = self.client.get("/pokemon/types/grass/ice")
         # print(response.get_json().get('Abomasnow').get('types'))
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Abomasnow').get('types') == ['grass', 'ice'])
 
+    # Test to get pokemon's status by input a hp level
     def test_get_pokemon_hp(self):
         response = self.client.get("/pokemon/stats/hp/50")
         # print(response.get_json())
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Aron').get('types') == ['rock', 'steel'])
 
+    # Test to get pokemon's status by input attack value
     def test_get_pokemon_attack(self):
         response = self.client.get("/pokemon/stats/attack/50")
         # print(response.get_json())
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Aegislash').get('types') == ['steel', 'ghost'])
 
+    # Test to get pokemon's status by input defense value
     def test_get_pokemon_defense(self):
         response = self.client.get("/pokemon/stats/defense/60")
         # print(response.get_json())
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Absol').get('types') == ['dark'])
 
+    # Test to get pokemon's status by input special_attack value
     def test_get_pokemon_special_attack(self):
         response = self.client.get("/pokemon/stats/special_attack/50")
         # print(response.get_json())
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Aegislash').get('types') == ['steel', 'ghost'])
 
+    # Test to get pokemon's status by input special_defense value
     def test_get_pokemon_special_defense(self):
         response = self.client.get("/pokemon/stats/special_defense/50")
         # print(response.get_json())
         self.assertTrue(200, response.status_code)
         self.assertTrue(response.get_json().get('Anorith').get('types') == ['bug', 'rock'])
 
+    # Test to get pokemon's status by input a speed
     def test_get_pokemon_speed(self):
         response = self.client.get("/pokemon/stats/speed/50")
         # print(response.get_json())
